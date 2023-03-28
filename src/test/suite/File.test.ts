9f9e5ba33b883f3dcb1ba0d326be068eea4bab87
Norbert Klein <nklein@nkode.de>
2023-03-28 13:49:43 +0200

    new tests for Ring.delete - mocha test learning

--- src/test/suite/File.test.ts
+++ src/test/suite/File.test.ts
@@ -1,285 +1,283 @@
 import { describe, it, beforeEach } from 'mocha';
 import { expect } from 'chai';
+import { vscode } from '../mock/vscode.mock';
 
+import { IPersistFile, IPersistMark } from '../../types';
 import { File } from '../../File';
 import { Mark } from '../../Mark';
-import { IPersistFile, IPersistMark } from '../../types';
-
-let firstFile: File;
 
 describe('File', () => {
-  const filePath = 'test.js';
-  const lineNumber = 1;
-  const label = 'test mark';
-  let file: File;
+	const filePath = 'test.js';
+	const lineNumber = 1;
+	const label = 'test mark';
+	let firstFile: File;
 
-  beforeEach(() => {
-    firstFile = new File(filePath, lineNumber, label);
-  });
 
-  describe('allPersistMarks', () => {
-    it('should return an array of IPersistMark objects representing all marks in the file', () => {
-      const filePath = 'example/file/path';
-      const lineNumber1 = 1;
-      const label1 = 'Example Label 1';
-      //   const mark1 = new Mark(filePath, lineNumber1, label1);
-      const lineNumber2 = 5;
-      const label2 = 'Example Label 2';
-      //   const mark2 = new Mark(filePath, lineNumber2, label2);
-      const file = new File(filePath);
-      file.addMark({ lineNumber: lineNumber1, label: label1 });
-      file.addMark({ lineNumber: lineNumber2, label: label2 });
+	beforeEach(() => {
+		firstFile = new File(filePath, lineNumber, label);
+	});
 
-      const persistMarks = file.allPersistMarks;
+	describe('allPersistMarks', () => {
+		it('should return an array of IPersistMark objects representing all marks in the file', () => {
+			const filePath = 'example/file/path';
+			const lineNumber1 = 1;
+			const label1 = 'Example Label 1';
+			const lineNumber2 = 5;
+			const label2 = 'Example Label 2';
+			const file = new File(filePath);
+			file.addMark({ lineNumber: lineNumber1, label: label1 });
+			file.addMark({ lineNumber: lineNumber2, label: label2 });
 
-      expect(persistMarks).to.eql([
-        { lineNumber: lineNumber1, label: label1 },
-        { lineNumber: lineNumber2, label: label2 },
-      ]);
-    });
-  });
+			const persistMarks = file.allPersistMarks;
 
-  describe('quickPickItems', () => {
-    it('returns an empty array when there are no marks with quickPickItem', () => {
-      const file = new File('test.ts');
-      expect(file.quickPickItems).to.eql([]);
-    });
+			expect(persistMarks).to.eql([
+				{ lineNumber: lineNumber1, label: label1 },
+				{ lineNumber: lineNumber2, label: label2 },
+			]);
+		});
+	});
 
-    // todo test quickPickItems
-    // it('returns an array of quickPickItems when there are marks with quickPickItem', () => {
-    //   const file = new File('test.ts');
-    //   const mark1 = new Mark('test.ts', 1, 'Mark 1');
-    //   const mark2 = new Mark('test.ts', 2, 'Mark 2');
-    //   file.mergeMarksAndLineNumbers([
-    //     { lineNumber: mark1.lineNumber, label: mark1.label },
-    //     { lineNumber: mark2.lineNumber, label: mark2.label },
-    //   ]);
+	describe('quickPickItems', () => {
+		it('returns an empty array when there are no marks with quickPickItem', () => {
+			const file = new File('test.ts');
+			expect(file.quickPickItems).to.eql([]);
+		});
 
-    //   expect(file.quickPickItems).to.eql([
-    //     mark1.quickPickItem,
-    //     mark2.quickPickItem,
-    //   ]);
-    // });
-  });
+		// todo test quickPickItems
+		// it('returns an array of quickPickItems when there are marks with quickPickItem', () => {
+		//   const file = new File('test.ts');
+		//   const mark1 = new Mark('test.ts', 1, 'Mark 1');
+		//   const mark2 = new Mark('test.ts', 2, 'Mark 2');
+		//   file.mergeMarksAndLineNumbers([
+		//     { lineNumber: mark1.lineNumber, label: mark1.label },
+		//     { lineNumber: mark2.lineNumber, label: mark2.label },
+		//   ]);
 
-  describe('a File', () => {
-    it('should have a filepath and an empty number array', () => {
-      //   firstFile = new File(filePath, lineNumber, label);
-      expect(firstFile.filepath).to.eql('test.js');
-      expect(firstFile.lineNumbers).to.eql([1]);
-    });
-  });
+		//   expect(file.quickPickItems).to.eql([
+		//     mark1.quickPickItem,
+		//     mark2.quickPickItem,
+		//   ]);
+		// });
+	});
 
-  describe('constructor', () => {
-    it('should initialize with filepath and no marks if line number is not provided', () => {
-      const filePath = '/path/to/file.txt';
-      const file = new File(filePath);
-      expect(file.filepath).to.eql(filePath);
-      expect(file.marks).to.eql([]);
-    });
+	describe('a File', () => {
+		it('should have a filepath and an empty number array', () => {
+			//   firstFile = new File(filePath, lineNumber, label);
+			expect(firstFile.filepath).to.eql('test.js');
+			expect(firstFile.lineNumbers).to.eql([1]);
+		});
+	});
 
-    it('should initialize with filepath and a new mark if line number is provided', () => {
-      const filePath = '/path/to/file.txt';
-      const lineNumber = 10;
-      const label = 'Test label';
-      const file = new File(filePath, lineNumber, label);
-      expect(file.filepath).to.eql(filePath);
-      expect(file.marks).to.eql([new Mark(filePath, lineNumber, label)]);
-    });
-  });
+	describe('constructor', () => {
+		it('should initialize with filepath and no marks if line number is not provided', () => {
+			const filePath = '/path/to/file.txt';
+			const file = new File(filePath);
+			expect(file.filepath).to.eql(filePath);
+			expect(file.marks).to.eql([]);
+		});
 
-  describe('mergeMarksAnd_PersistFile_', () => {
-    it('should return the same file if persistFile is undefined', () => {
-      const filePath = '/path/to/file.txt';
-      const file = new File(filePath);
-      const persistFile = undefined;
-      const mergedFile = file.mergeMarksAnd_PersistFile_(
-        persistFile as unknown as IPersistFile
-      );
-      expect(mergedFile).to.eql(file);
-    });
+		it('should initialize with filepath and a new mark if line number is provided', () => {
+			const filePath = '/path/to/file.txt';
+			const lineNumber = 10;
+			const label = 'Test label';
+			const file = new File(filePath, lineNumber, label);
+			expect(file.filepath).to.eql(filePath);
+			expect(file.marks).to.eql([new Mark(filePath, lineNumber, label)]);
+		});
+	});
 
-    it('should merge the persist marks with existing marks in the file', () => {
-      const filePath = '/path/to/file.txt';
-      const file = new File(filePath);
-      const persistMarks = [
-        { lineNumber: 10, label: 'Test label 1' },
-        { lineNumber: 20, label: 'Test label 2' },
-      ] as IPersistMark[];
-      const mergedFile = file.mergeMarksAnd_PersistFile_({
-        filepath: '',
-        persistMarks: persistMarks,
-      });
-      expect(mergedFile.marks).to.eql([
-        new Mark(filePath, 10, 'Test label 1'),
-        new Mark(filePath, 20, 'Test label 2'),
-      ]);
-    });
-  });
+	describe('mergeMarksAnd_PersistFile_', () => {
+		it('should return the same file if persistFile is undefined', () => {
+			const filePath = '/path/to/file.txt';
+			const file = new File(filePath);
+			const persistFile = undefined;
+			const mergedFile = file.mergeMarksAnd_PersistFile_(
+				persistFile as unknown as IPersistFile
+			);
+			expect(mergedFile).to.eql(file);
+		});
 
-  describe('addMark', () => {
-    it('should add a new mark to the file', () => {
-      const filePath = '/path/to/file.txt';
-      const file = new File(filePath);
-      const lineNumber = 10;
-      const label = 'Test label';
-      file.addMark({ lineNumber, label });
-      expect(file.marks).to.eql([new Mark(filePath, lineNumber, label)]);
-    });
-  });
+		it('should merge the persist marks with existing marks in the file', () => {
+			const filePath = '/path/to/file.txt';
+			const file = new File(filePath);
+			const persistMarks = [
+				{ lineNumber: 10, label: 'Test label 1' },
+				{ lineNumber: 20, label: 'Test label 2' },
+			] as IPersistMark[];
+			const mergedFile = file.mergeMarksAnd_PersistFile_({
+				filepath: '',
+				persistMarks: persistMarks,
+			});
+			expect(mergedFile.marks).to.eql([
+				new Mark(filePath, 10, 'Test label 1'),
+				new Mark(filePath, 20, 'Test label 2'),
+			]);
+		});
+	});
 
-  describe('hasMark', () => {
-    it('should return true if a mark with the given line number exists in the file', () => {
-      const filePath = '/path/to/file.txt';
-      const lineNumber = 10;
-      const label = 'Test label';
-      const file = new File(filePath, lineNumber, label);
-      expect(file.hasMark(lineNumber)).to.eql(true);
-    });
+	describe('addMark', () => {
+		it('should add a new mark to the file', () => {
+			const filePath = '/path/to/file.txt';
+			const file = new File(filePath);
+			const lineNumber = 10;
+			const label = 'Test label';
+			file.addMark({ lineNumber, label });
+			expect(file.marks).to.eql([new Mark(filePath, lineNumber, label)]);
+		});
+	});
 
-    it('should return false if a mark with the given line number does not exist in the file', () => {
-      const filePath = '/path/to/file.txt';
-      const lineNumber = 10;
-      const label = 'Test label';
-      const file = new File(filePath, lineNumber, label);
-      expect(file.hasMark(20)).to.eql(false);
-    });
-  });
+	describe('hasMark', () => {
+		it('should return true if a mark with the given line number exists in the file', () => {
+			const filePath = '/path/to/file.txt';
+			const lineNumber = 10;
+			const label = 'Test label';
+			const file = new File(filePath, lineNumber, label);
+			expect(file.hasMark(lineNumber)).to.eql(true);
+		});
 
-  describe('toggleTaskMark', () => {
-    it('should remove a mark if it already exists in the file', () => {
-      const filePath = '/path/to/file.txt';
-      const lineNumber = 10;
-      const label = 'Test label';
-      const file = new File(filePath, lineNumber, label);
-      expect(file.marks).to.eql([new Mark(filePath, lineNumber, label)]);
-      file.toggleTaskMark({ lineNumber, label: '' });
-      expect(file.marks).to.eql([]);
-    });
-  });
+		it('should return false if a mark with the given line number does not exist in the file', () => {
+			const filePath = '/path/to/file.txt';
+			const lineNumber = 10;
+			const label = 'Test label';
+			const file = new File(filePath, lineNumber, label);
+			expect(file.hasMark(20)).to.eql(false);
+		});
+	});
 
-  describe('after adding an array of lineNumbers (numbers)', () => {
-    it('lineNumbers should return these numbers (ordered, no doubles)', () => {
-      firstFile.mergeMarksAndLineNumbers([
-        { lineNumber: 20, label: '' },
-        { lineNumber: 30, label: '' },
-        { lineNumber: 10, label: '' },
-        { lineNumber: 20, label: '' },
-        { lineNumber: 30, label: '' },
-      ]);
-      expect(firstFile.allPathMarks).to.eql([
-        {
-          filepath: 'test.js',
-          label: 'test mark',
-          lineNumber: 1,
-        },
-        { filepath: 'test.js', lineNumber: 10, label: '' },
-        { filepath: 'test.js', lineNumber: 20, label: '' },
-        { filepath: 'test.js', lineNumber: 30, label: '' },
-      ]);
-    });
-  });
+	describe('toggleTaskMark', () => {
+		it('should remove a mark if it already exists in the file', () => {
+			const filePath = '/path/to/file.txt';
+			const lineNumber = 10;
+			const label = 'Test label';
+			const file = new File(filePath, lineNumber, label);
+			expect(file.marks).to.eql([new Mark(filePath, lineNumber, label)]);
+			file.toggleTaskMark({ lineNumber, label: '' });
+			expect(file.marks).to.eql([]);
+		});
+	});
 
-  it('after adding a second array of lineNumbers (numbers), lineNumbers should return the combined numbers', () => {
-    firstFile.mergeMarksAndLineNumbers([
-      { lineNumber: 20, label: '' },
-      { lineNumber: 30, label: '' },
-      { lineNumber: 10, label: '' },
-      { lineNumber: 20, label: '' },
-      { lineNumber: 30, label: '' },
-    ]);
-    firstFile.mergeMarksAndLineNumbers([
-      { lineNumber: 20, label: '' },
-      { lineNumber: 50, label: '' },
-      { lineNumber: 40, label: '' },
-      { lineNumber: 50, label: '' },
-    ]);
-    expect(firstFile.lineNumbers).to.eql([1, 10, 20, 30, 40, 50]);
-    expect(firstFile.hasMarks).to.eql(true);
-  });
+	describe('after adding an array of lineNumbers (numbers)', () => {
+		it('lineNumbers should return these numbers (ordered, no doubles)', () => {
+			firstFile.mergeMarksAndLineNumbers([
+				{ lineNumber: 20, label: '' },
+				{ lineNumber: 30, label: '' },
+				{ lineNumber: 10, label: '' },
+				{ lineNumber: 20, label: '' },
+				{ lineNumber: 30, label: '' },
+			]);
+			expect(firstFile.allPathMarks).to.eql([
+				{
+					filepath: 'test.js',
+					label: 'test mark',
+					lineNumber: 1,
+				},
+				{ filepath: 'test.js', lineNumber: 10, label: '' },
+				{ filepath: 'test.js', lineNumber: 20, label: '' },
+				{ filepath: 'test.js', lineNumber: 30, label: '' },
+			]);
+		});
+	});
 
-  it('after adding an existing lineNumber, nothing should change (ordered, no doubles)', () => {
-    firstFile.mergeMarksAndLineNumbers([
-      { lineNumber: 20, label: '' },
-      { lineNumber: 30, label: '' },
-      { lineNumber: 10, label: '' },
-      { lineNumber: 20, label: '' },
-      { lineNumber: 30, label: '' },
-    ]);
-    firstFile.mergeMarksAndLineNumbers([
-      { lineNumber: 20, label: '' },
-      { lineNumber: 50, label: '' },
-      { lineNumber: 40, label: '' },
-      { lineNumber: 50, label: '' },
-    ]);
-    firstFile.addMark({ lineNumber: 30, label: '' });
-    expect(firstFile.lineNumbers).to.eql([1, 10, 20, 30, 40, 50]);
-  });
+	it('after adding a second array of lineNumbers (numbers), lineNumbers should return the combined numbers', () => {
+		firstFile.mergeMarksAndLineNumbers([
+			{ lineNumber: 20, label: '' },
+			{ lineNumber: 30, label: '' },
+			{ lineNumber: 10, label: '' },
+			{ lineNumber: 20, label: '' },
+			{ lineNumber: 30, label: '' },
+		]);
+		firstFile.mergeMarksAndLineNumbers([
+			{ lineNumber: 20, label: '' },
+			{ lineNumber: 50, label: '' },
+			{ lineNumber: 40, label: '' },
+			{ lineNumber: 50, label: '' },
+		]);
+		expect(firstFile.lineNumbers).to.eql([1, 10, 20, 30, 40, 50]);
+		expect(firstFile.hasMarks).to.eql(true);
+	});
 
-  it('after adding one new lineNumber, lineNumbers should return these numbers (ordered, no doubles)', () => {
-    firstFile.mergeMarksAndLineNumbers([
-      { lineNumber: 20, label: '' },
-      { lineNumber: 30, label: '' },
-      { lineNumber: 10, label: '' },
-      { lineNumber: 20, label: '' },
-      { lineNumber: 30, label: '' },
-    ]);
-    firstFile.mergeMarksAndLineNumbers([
-      { lineNumber: 20, label: '' },
-      { lineNumber: 50, label: '' },
-      { lineNumber: 40, label: '' },
-      { lineNumber: 50, label: '' },
-    ]);
-    firstFile.addMark({ lineNumber: 25, label: '' });
-    expect(firstFile.lineNumbers).to.eql([1, 10, 20, 25, 30, 40, 50]);
-  });
+	it('after adding an existing lineNumber, nothing should change (ordered, no doubles)', () => {
+		firstFile.mergeMarksAndLineNumbers([
+			{ lineNumber: 20, label: '' },
+			{ lineNumber: 30, label: '' },
+			{ lineNumber: 10, label: '' },
+			{ lineNumber: 20, label: '' },
+			{ lineNumber: 30, label: '' },
+		]);
+		firstFile.mergeMarksAndLineNumbers([
+			{ lineNumber: 20, label: '' },
+			{ lineNumber: 50, label: '' },
+			{ lineNumber: 40, label: '' },
+			{ lineNumber: 50, label: '' },
+		]);
+		firstFile.addMark({ lineNumber: 30, label: '' });
+		expect(firstFile.lineNumbers).to.eql([1, 10, 20, 30, 40, 50]);
+	});
 
-  describe('toggleTaskMark', () => {
-    it('should add mark', () => {
-      const lineNumber = 20;
-      const label = 'test 20';
-      const firstFile5 = new File(filePath, lineNumber, label);
+	it('after adding one new lineNumber, lineNumbers should return these numbers (ordered, no doubles)', () => {
+		firstFile.mergeMarksAndLineNumbers([
+			{ lineNumber: 20, label: '' },
+			{ lineNumber: 30, label: '' },
+			{ lineNumber: 10, label: '' },
+			{ lineNumber: 20, label: '' },
+			{ lineNumber: 30, label: '' },
+		]);
+		firstFile.mergeMarksAndLineNumbers([
+			{ lineNumber: 20, label: '' },
+			{ lineNumber: 50, label: '' },
+			{ lineNumber: 40, label: '' },
+			{ lineNumber: 50, label: '' },
+		]);
+		firstFile.addMark({ lineNumber: 25, label: '' });
+		expect(firstFile.lineNumbers).to.eql([1, 10, 20, 25, 30, 40, 50]);
+	});
 
-      const persistMark = { lineNumber: 10, label: 'test 10' };
-      firstFile5.toggleTaskMark(persistMark);
+	describe('toggleTaskMark', () => {
+		it('should add mark', () => {
+			const lineNumber = 20;
+			const label = 'test 20';
+			const firstFile5 = new File(filePath, lineNumber, label);
 
-      expect(firstFile5.hasMark(lineNumber)).to.be.true;
+			const persistMark = { lineNumber: 10, label: 'test 10' };
+			firstFile5.toggleTaskMark(persistMark);
 
-      const mark = firstFile5.marks[0];
-      expect(mark.lineNumber).to.equal(10);
-      expect(mark.label).to.equal('test 10');
-    });
+			expect(firstFile5.hasMark(lineNumber)).to.be.true;
 
-    it('should remove mark if exists', () => {
-      const lineNumber = 10;
-      const label = 'test';
-      const persistMark = { lineNumber, label };
+			const mark = firstFile5.marks[0];
+			expect(mark.lineNumber).to.equal(10);
+			expect(mark.label).to.equal('test 10');
+		});
 
-      const firstFile6 = new File(filePath, lineNumber, label);
-      //   firstFile6.addMark(persistMark);
+		it('should remove mark if exists', () => {
+			const lineNumber = 10;
+			const label = 'test';
+			const persistMark = { lineNumber, label };
 
-      firstFile6.toggleTaskMark(persistMark);
+			const firstFile6 = new File(filePath, lineNumber, label);
+			//   firstFile6.addMark(persistMark);
 
-      expect(firstFile6.hasMark(lineNumber)).to.be.false;
-      expect(firstFile6.marks.length).to.equal(0);
-    });
-  });
+			firstFile6.toggleTaskMark(persistMark);
 
-  describe('hasMarks', () => {
-    it('should return false if there are no marks', () => {
-      const firstFile2 = new File(filePath);
-      expect(firstFile2.hasMarks).to.be.false;
-    });
+			expect(firstFile6.hasMark(lineNumber)).to.be.false;
+			expect(firstFile6.marks.length).to.equal(0);
+		});
+	});
 
-    it('should return true if there are marks', () => {
-      const lineNumber = 10;
-      const label = 'test';
-      const persistMark = { lineNumber, label };
+	describe('hasMarks', () => {
+		it('should return false if there are no marks', () => {
+			const firstFile2 = new File(filePath);
+			expect(firstFile2.hasMarks).to.be.false;
+		});
 
-      const firstFile3 = new File(filePath, lineNumber, label);
-      firstFile3.addMark(persistMark);
+		it('should return true if there are marks', () => {
+			const lineNumber = 10;
+			const label = 'test';
+			const persistMark = { lineNumber, label };
 
-      expect(firstFile3.hasMarks).to.be.true;
-    });
-  });
+			const firstFile3 = new File(filePath, lineNumber, label);
+			firstFile3.addMark(persistMark);
+
+			expect(firstFile3.hasMarks).to.be.true;
+		});
+	});
 });
--- src/test/suite/Mark.test.ts
+++ src/test/suite/Mark.test.ts
@@ -8,64 +8,62 @@ import * as sinon from 'sinon';
 import { vscode } from '../mock/vscode.mock';
 
 suite('Mark', () => {
-  test('Constructor', () => {
-    const filepath = 'src/test.ts';
-    const lineNumber = 5;
-    const label = 'My mark';
-    const mark = new Mark(filepath, lineNumber, label);
-    assert.strictEqual(mark.filepath, filepath);
-    assert.strictEqual(mark.lineNumber, lineNumber);
-    assert.strictEqual(mark.label, label);
-  });
+	test('Constructor', () => {
+		const filepath = 'src/test.ts';
+		const lineNumber = 5;
+		const label = 'My mark';
+		const mark = new Mark(filepath, lineNumber, label);
+		assert.strictEqual(mark.filepath, filepath);
+		assert.strictEqual(mark.lineNumber, lineNumber);
+		assert.strictEqual(mark.label, label);
+	});
 
-  it('should call myFunction once', () => {
-    const myFunction = sinon.spy();
-    myFunction();
-    sinon.assert.calledOnce(myFunction);
-  });
+	it('should call myFunction once', () => {
+		const myFunction = sinon.spy();
+		myFunction();
+		sinon.assert.calledOnce(myFunction);
+	});
 
-  // todo
-  // test('setQuickPickItem', async () => {
-  // 	const filepath = 'src/test.ts';
-  // 	const lineNumber = 5;
-  // 	const label = 'My mark';
-  // 	const mark = new Mark(filepath, lineNumber, label);
-  // 	// await mark.setQuickPickItem(filepath, lineNumber, label);
-  // 	assert.strictEqual(mark.quickPickItem?.label, label);
-  // 	assert.strictEqual(mark.quickPickItem?.detail, `${filepath}:${lineNumber}`);
-  // 	assert.strictEqual(mark.quickPickItem?.description, '');
-  // });
+	// todo
+	// test('setQuickPickItem', async () => {
+	// 	const filepath = 'src/test.ts';
+	// 	const lineNumber = 5;
+	// 	const label = 'My mark';
+	// 	const mark = new Mark(filepath, lineNumber, label);
+	// 	// await mark.setQuickPickItem(filepath, lineNumber, label);
+	// 	assert.strictEqual(mark.quickPickItem?.label, label);
+	// 	assert.strictEqual(mark.quickPickItem?.detail, `${filepath}:${lineNumber}`);
+	// 	assert.strictEqual(mark.quickPickItem?.description, '');
+	// });
 });
 
 describe('My Extension', () => {
-  let mockVscode: typeof vscode; // = {}
+	it('should do something', async () => {
+		// Use the fake vscode methods here
+		vscode.commands.executeCommand('myCommand');
+		expect(vscode.window.showInformationMessage.calledWith('Hello, World!'));
+	});
 
-  it('should do something', async () => {
-    // Use the fake vscode methods here
-    vscode.commands.executeCommand('myCommand');
-    expect(vscode.window.showInformationMessage.calledWith('Hello, World!'));
-  });
+	test('Test something that requires vscode', async () => {
+		// Create a mock of the workspace module
+		const mockWorkspace = {
+			openTextDocument: async (path: string) => {
+				const text = 'Hello, World!';
+				return {
+					getText: () => text,
+					lineCount: text.split('\n').length,
+					uri: vscode.Uri.file(path),
+				};
+			},
+		};
+		// mockVscode.workspace = mockWorkspace as any;
 
-  test('Test something that requires vscode', async () => {
-    // Create a mock of the workspace module
-    const mockWorkspace = {
-      openTextDocument: async (path: string) => {
-        const text = 'Hello, World!';
-        return {
-          getText: () => text,
-          lineCount: text.split('\n').length,
-          uri: vscode.Uri.file(path),
-        };
-      },
-    };
-    // mockVscode.workspace = mockWorkspace as any;
+		// Call your extension function that requires vscode.workspace.openTextDocument
+		const doc = await mockWorkspace.openTextDocument('/path/to/document.txt');
 
-    // Call your extension function that requires vscode.workspace.openTextDocument
-    const doc = await mockWorkspace.openTextDocument('/path/to/document.txt');
-
-    // Make your assertions
-    assert.equal(doc.lineCount, 1);
-    assert.equal(doc.getText(), 'Hello, World!');
-    // assert.equal(doc.uri, '/path/to/document.txt');
-  });
+		// Make your assertions
+		assert.equal(doc.lineCount, 1);
+		assert.equal(doc.getText(), 'Hello, World!');
+		// assert.equal(doc.uri, '/path/to/document.txt');
+	});
 });
