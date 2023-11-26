83f4a9b80ae027f4860b21a65d83072aa4eb0663
Norbert Klein <nklein@nkode.de>
2023-11-26 18:10:09 +0100

    better ring and tests for ring

--- src/Ring.ts
+++ src/Ring.ts
@@ -1,16 +1,32 @@
 // not a 'real' ring class - only stuff I need
+// current is the current item or undefined, if the ring is empty
+// if a new item is inserted, it becomes the current item
+// if the current item is deleted, the next item becomes the current item
+// if the current item is deleted and there is no next item, the previous item becomes the current item
+// if the current item is deleted and it was the last item in the ring, the ring becomes empty
+
 export class Ring<T> extends Array<T> {
-	private _current = -1;
+	private _current: number = -1;
 
-	//   constructor(items: T[] = []) {
-	//     super(...items);
-	//   }
+	constructor(...items: T[]) {
+		super(...items);
+		this._current = this.length - 1;
+	}
 
-	get current(): T | undefined {
-		if (this._current === -1) {
-			return undefined;
-		}
+	get currentPosition(): number {
+		return this._current;
+	}
+
+	set currentPosition(position: number) {
+		this._current = (position + this.length) % this.length;
+	}
+
+	get current(): T {
 		return this[this._current];
+	}
+
+	set current(item: T) {
+		this[this._current] = item;
 	}
 
 	get empty(): boolean {
@@ -18,79 +34,95 @@ export class Ring<T> extends Array<T> {
 	}
 
 	get next(): T | undefined {
-		this._current++;
-		this.checkPosition();
+		if (this.length === 0) {
+			return undefined;
+		}
+		this.currentPosition = this._current + 1;
 		return this.current;
 	}
 
 	get previous(): T | undefined {
-		this._current--;
-		this.checkPosition();
+		if (this.length === 0) {
+			return undefined;
+		}
+		this.currentPosition = this._current - 1;
 		return this.current;
 	}
 
-	get top(): T | undefined {
+	get first(): T | undefined {
+		if (this.length === 0) {
+			return undefined;
+		}
 		this._current = 0;
 		return this.current;
 	}
 
-	get bottom(): T | undefined {
-		this._current = this.length - 1;
-		return this.current;
-	}
-
-	private checkPosition(): void {
-		if (this._current === -1) {
-			this._current = this.length - 1;
-		} else {
-			if (this._current === this.length) {
-				this._current = 0;
-			}
+	get last(): T | undefined {
+		if (this.length === 0) {
+			return undefined;
 		}
-	}
-
-	insertBefore(item: T): T | undefined {
-		super.splice(this._current, 0, item);
+		this._current = this.length - 1;
 		return this.current;
 	}
 
-	insertAfter(item: T): T | undefined {
-		this._current++;
-		super.splice(this._current, 0, item);
+	insertBefore(item: T): T {
+		if (this.length === 0) {
+			this.push(item);
+		} else {
+			this.splice(this._current, 0, item);
+		}
 		return this.current;
 	}
 
-	pushArray(items: T[]): number {
-		super.push(...items);
-		this._current = this.length - 1;
-		return items.length;
+	insertAfter(item: T): T {
+		if (this.length === 0) {
+			this.push(item);
+		} else {
+			this.splice(this._current + 1, 0, item);
+			this._current = (this._current + 1) % this.length;
+		}
+		return this.current;
 	}
 
-	push(item: T): number {
-		super.push(item);
-		this._current = this.length - 1;
-		return 1;
+	push(...items: T[]): number {
+		this._current = super.push(...items) - 1;
+		return this.length;
 	}
 
 	deleteCurrent(): T | undefined {
-		super.splice(this._current, 1)[0];
-		this.checkPosition();
+		if (this.length === 0) {
+			return undefined;
+		}
+		this.splice(this._current, 1);
+		this._current = this._current % this.length;
+		return this.current;
+	}
+
+	popCurrent(): T | undefined {
+		if (this.length === 0) {
+			return undefined;
+		}
+
 		const current = this.current;
+		this.deleteCurrent();
 		return current;
 	}
 
-	delete(key: T): void {
-		const index = super.indexOf(key, 0);
-		if (index > -1) {
-			super.splice(index, 1);
-		}
+	delete(key: T): boolean {
 		if (this.length === 0) {
-			this._current = -1;
-			return;
+			return false;
 		}
-		if (this._current < this.length) {
-			return;
+
+		const index = this.indexOf(key);
+		if (index !== -1) {
+			this.splice(index, 1);
+			if (index <= this._current) {
+				this.currentPosition = this._current - 1;
+			}
+
+			return true;
 		}
-		this._current = this.length - 1;
+
+		return false;
 	}
 }
